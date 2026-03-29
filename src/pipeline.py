1a9e18bed2f477e52d67886fb242afbfb8327f36
Fekraddis Yalew <31040659@sun.ac.za>
2026-03-29 14:05:49 +0200

    Addition of quality control module with FASTQ

--- src/pipeline.py
+++ src/pipeline.py
@@ -1,1 +1,21 @@
 Bioinformatics pipeline assignment started
+
+#simple steps for QC modules
+
+import gzip
+
+file = "sample.fastq.gz" # works for gz files
+total_reads = 0
+total_length = 0
+with gzip.open(file, "rt") as f:
+    for i, line in enumerate(f):
+        if i % 4 == 1: # sequence line
+            seq = line.strip()
+            total_reads += 1
+            total_length += len(seq)
+if total_reads > 0:
+    print("Total reads:", total_reads)
+    print("Average length:", total_length / total_reads)
+else:
+    print("No reads found!")
+
